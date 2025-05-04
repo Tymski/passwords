30dbebe2520a2abf366538c4336a0e46b71d8723
Tymon Oziemblewski <tymon.oziemblewski@gmail.com>
2025-05-04 09:27:10 +0200

    Small refactor, password for websites with checks

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const password = document.getElementById('password');
 const passwordNumbers = document.getElementById('passwordNumbers');
 const passwordSymbols = document.getElementById('passwordSymbols');
 const passwordSymbols1 = document.getElementById('passwordSymbols1');
+const passwordSymbolsAdd = document.getElementById('passwordSymbolsAdd');
 const createPasswordButton = document.getElementById('createPasswordButton');
 
 const numbers = "0123456789";
@@ -9,6 +10,8 @@ const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const basicSymbols = "!#$%&*+-=?@^_()";
 const commonSymbols = numbers + lowerCaseLetters + upperCaseLetters + basicSymbols;
+
+const randomUintBuffer = new Uint32Array(1);
 
 function generatePassword() {
     var length = 30;
@@ -28,12 +31,6 @@ function generateNumber() {
     return password;
 }
 
-function generateInt() {
-    let array = new Int32Array(3);
-    crypto.getRandomValues(array);
-    return array.map(element => Math.abs(element)).join('');
-}
-
 function generateSymbols() {
     var password = "";
     for (var i = 0; i < 30; ++i) {
@@ -43,12 +40,12 @@ function generateSymbols() {
 }
 
 function getRandomCharacter(characters) {
-    const randomIndex = crypto.getRandomValues(new Uint32Array(1))[0] % characters.length;
+    const randomIndex = crypto.getRandomValues(randomUintBuffer)[0] % characters.length;
     return characters[randomIndex];
 }
 
 function getRandomSymbol() {
-    const symbol = crypto.getRandomValues(new Uint32Array(1))[0] % 100000 + 1000;
+    const symbol = crypto.getRandomValues(randomUintBuffer)[0] % 100000 + 1000;
     return "&#" + symbol + ";";
 }
 
@@ -57,14 +54,32 @@ function generatePasswordToClipboard(element) {
     element.innerHTML = generateSymbols();
 }
 
+function copyToClipboard(element) {
+    navigator.clipboard.writeText(element.innerHTML);
+}
+
+function addWebisteChecks(password) {
+    var prefix = getRandomCharacter(numbers);
+    prefix += getRandomCharacter(lowerCaseLetters);
+    prefix += getRandomCharacter(upperCaseLetters);
+    prefix += getRandomCharacter(basicSymbols);
+    return prefix + password;
+}
+
 function generate() {
     password.innerHTML = generatePassword();
     passwordNumbers.innerHTML = generateNumber();
     passwordSymbols.innerHTML = generateSymbols();
-    passwordSymbols1.innerHTML = generateSymbols();
+    passwordSymbols1.innerHTML = addWebisteChecks(generateSymbols());
+    passwordSymbolsAdd.innerHTML = addWebisteChecks(generateSymbols());
 }
 
 createPasswordButton.onclick = () => generatePasswordToClipboard(passwordSymbols1);
+password.onclick = () => copyToClipboard(password);
+passwordNumbers.onclick = () => copyToClipboard(passwordNumbers);
+passwordSymbols.onclick = () => copyToClipboard(passwordSymbols);
+passwordSymbols1.onclick = () => copyToClipboard(passwordSymbols1);
+passwordSymbolsAdd.onclick = () => copyToClipboard(passwordSymbolsAdd);
 generate();
 
 setInterval(generate, 500);
